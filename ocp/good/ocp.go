0c183cacb002f90405a8746e2e16ab9403c40b38
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-14 10:23:36 +0300

    change structure

---
+++ ocp/good/ocp.go
@@ -0,0 +1,24 @@
+package good
+
+type entityA struct {
+	paramA int
+	paramB int
+}
+
+type entityB struct {
+	paramC int
+}
+
+// open for expansion (new types of entity might be implemented through interface)
+// closed for modification (doesn't need to change the total() function after adding each new entity).
+type entity interface {
+	total() int
+}
+
+func (e *entityA) total() int {
+	return e.paramA * e.paramB
+}
+
+func (e *entityB) total() int {
+	return e.paramC * 2
+}
