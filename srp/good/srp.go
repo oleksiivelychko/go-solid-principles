cf2bb307ee68af1c0625fd3580185bf17187a1e6
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-14 09:55:30 +0300

    edit comment style

--- srp/good/srp.go
+++ srp/good/srp.go
@@ -4,18 +4,14 @@ package good
 Single responsibility in right way.
 */
 
-/*
-entity has only one responsibility: managing data.
-*/
+// entity has only one responsibility: managing data.
 type entity struct{}
 
 func (e *entity) getData() {
 	// managing data
 }
 
-/*
-entityRepository has only one responsibility: saving data.
-*/
+// entityRepository has only one responsibility: saving data.
 type entityRepository struct {
 	// database connection or other storage-related fields
 }
