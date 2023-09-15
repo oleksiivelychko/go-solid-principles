67bf32faeb7a392b6d661dbea4884d6a16ae94c3
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-15 09:00:58 +0300

    liskov substitution - bad

---
+++ lsp/bad/lsp.go
@@ -0,0 +1,18 @@
+package bad
+
+type base interface {
+	actionA() string
+}
+
+type derivedA struct{}
+
+func (d *derivedA) actionA() string {
+	return "derivedA is doing actionA."
+}
+
+// derivedB is not intended for actionA() and violates the principle.
+type derivedB struct{}
+
+func (d *derivedB) actionA() string {
+	return "derivedB is doing actionA."
+}
