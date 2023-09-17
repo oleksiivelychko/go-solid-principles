80bd93ee8e272a1cb1bc9a83169b5842ae0ef93d
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-09-17 10:10:32 +0300

    edit order of types

--- ocp/good/main.go
+++ ocp/good/main.go
@@ -1,5 +1,11 @@
 package main
 
+// open for expansion (new types of iEntity might be implemented through interface)
+// closed for modification (doesn't need to change the total() after adding each new iEntity).
+type iEntity interface {
+	total() int
+}
+
 type entityA struct {
 	paramA int
 	paramB int
@@ -7,12 +13,6 @@ type entityA struct {
 
 type entityB struct {
 	paramC int
-}
-
-// open for expansion (new types of iEntity might be implemented through interface)
-// closed for modification (doesn't need to change the total() function after adding each new iEntity).
-type iEntity interface {
-	total() int
 }
 
 func (e *entityA) total() int {
