84fa147cf482cb96ad885db3aae0526308ce3f28
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-04 08:30:19 +0200

    simplify error message

--- dip/good/main_test.go
+++ dip/good/main_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestDependencyInversion(t *testing.T) {
 	s := &serviceC{&serviceA{}}
 	if s.action() != "A" {
-		t.Error("got incorrect service implementation")
+		t.Error("incorrect implementation")
 	}
 
 	s = &serviceC{&serviceB{}}
 	if s.action() != "B" {
-		t.Error("got incorrect service implementation")
+		t.Error("incorrect implementation")
 	}
 }
