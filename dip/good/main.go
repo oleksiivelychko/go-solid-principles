83d4fbe931f6b7d178c789fbc84aa2f8defa6112
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 10:04:35 +0200

    simplify

--- dip/good/main.go
+++ dip/good/main.go
@@ -6,13 +6,9 @@ type serviceA struct{}
 type serviceB struct{}
 
 // serviceC depends on service (abstraction)
-type serviceC struct {
-	service service
-}
+type serviceC struct{ service service }
 
 func (s *serviceA) action() string { return "A" }
 func (s *serviceB) action() string { return "B" }
 
-func (s *serviceC) action() string {
-	return s.service.action()
-}
+func (s *serviceC) action() string { return s.service.action() }
