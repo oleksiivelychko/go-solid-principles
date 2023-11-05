9b19f4d0bff0af7d689b5d0ac35b4baa9f8d5209
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-11-05 10:05:01 +0200

    simplify

--- dip/good/main_test.go
+++ dip/good/main_test.go
@@ -5,14 +5,12 @@ import "testing"
 func TestDependencyInversion(t *testing.T) {
 	s := &serviceC{&serviceA{}}
 	got := s.action()
-
 	if got != "A" {
 		t.Errorf("expected %q, got %q", "A", got)
 	}
 
 	s = &serviceC{&serviceB{}}
 	got = s.action()
-
 	if got != "B" {
 		t.Errorf("expected %q, got %q", "B", got)
 	}
